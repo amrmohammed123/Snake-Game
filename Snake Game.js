31a01942dd9d3a44c7fd5d4460ff841d4bf46e0f
Amr Mohammed <amrmohammed362@gmail.com>
2018-01-18 19:18:07 +0200

    snake moving correctly and the fruit is generated (the first one)

--- Snake Game.js
+++ Snake Game.js
@@ -1,7 +1,8 @@
 $(document).ready(function()
 {
-	var direction = "up" , score = 0 , snakeXPositions = [], snakeYPositions = [] , time = 50
-	, SNAKE_WIDTH = 15 , SNAKE_HEIGHT = 15 , SNAKE_COLOR = "#0F0" , FRUIT_COLOR = "#F00";
+	var direction = "up" , score = 0 , snakeXPositions = [], snakeYPositions = [] , time = 50 , j = 0 , position
+	, SNAKE_WIDTH = 15 , SNAKE_HEIGHT = 15 , SNAKE_COLOR = "#0F0" , FRUIT_COLOR = "#F00" , i = 0 , emptyKeys = []
+	,xPosition = 0 , yPosition = 0 , t = 0;	
 	//set the width and height of the gameArea to the width and height of the user's window
 	var gameArea = document.getElementById("gameArea");
 	gameArea.width = $(window).innerWidth();
@@ -9,12 +10,37 @@ $(document).ready(function()
 	//draw the snake in the center of the gameArea
 	var context = gameArea.getContext("2d");
 	context.fillStyle = SNAKE_COLOR;
-	for(var i = 0 ; i < 6 ; i++)
+	//generate empty cells positions (for fruits)
+	var emptyCells = {};
+	var xLimit = Math.floor(gameArea.width / SNAKE_WIDTH);
+	var yLimit = Math.floor(gameArea.height / SNAKE_HEIGHT);
+	for(i = 1 ; i < xLimit ; i++)
 	{
-		context.fillRect(gameArea.width/2 , gameArea.height/2 - i * SNAKE_HEIGHT , SNAKE_WIDTH , SNAKE_HEIGHT);
-		snakeYPositions.push(gameArea.height/2 - i * SNAKE_HEIGHT);
-		snakeXPositions.push(gameArea.width/2);
+		for(j = 1 ; j < yLimit ; j++)
+		{
+			emptyCells[((i * 15) + "-" + (j * 15))] = true;	
+		}
+	}
+	console.log(Object.keys(emptyCells));
+	for(i = 0 ; i < 6 ; i++)
+	{
+		context.fillRect(Math.floor(Math.floor(gameArea.width/2) / SNAKE_WIDTH) * SNAKE_WIDTH 
+						 , Math.floor(Math.floor(gameArea.height/2)/SNAKE_HEIGHT)*SNAKE_HEIGHT - i * SNAKE_HEIGHT 
+						 , SNAKE_WIDTH , SNAKE_HEIGHT);
+		delete emptyCells[(Math.floor(Math.floor(gameArea.width/2) / SNAKE_WIDTH) * SNAKE_WIDTH) + "-" 
+						  + (Math.floor(Math.floor(gameArea.height/2)/SNAKE_HEIGHT)*SNAKE_HEIGHT - i * SNAKE_HEIGHT)];
+		snakeYPositions.push(Math.floor(Math.floor(gameArea.height/2)/SNAKE_HEIGHT)*SNAKE_HEIGHT - i * SNAKE_HEIGHT);
+		snakeXPositions.push(Math.floor(Math.floor(gameArea.width/2)/SNAKE_WIDTH)*SNAKE_WIDTH);
 	}	
+	console.log(Object.keys(emptyCells));
+	//generate a fruit in a random place
+	emptyKeys = Object.keys(emptyCells);	
+	position = emptyKeys[Math.floor(Math.random() * emptyKeys.length)]; //random valid position
+	console.log(position);
+	t = position.indexOf("-");
+	xPosition = position.substring(0,t);
+	yPosition = position.substring(t+1);
+	drawFruit(xPosition,yPosition);
 	//move the snake 20 times a second in the current direction
 	setTimeout(move,time);
 	//move the snake with the event of pressing an arrow key
@@ -26,6 +52,7 @@ $(document).ready(function()
 			if(direction == "left" || direction == "right")
 				return;
 			context.clearRect(0,0,gameArea.width,gameArea.height);
+			drawFruit(xPosition,yPosition);
 			snakeXPositions.shift();
 			snakeYPositions.shift();
 			redrawSnake();
@@ -57,6 +84,7 @@ $(document).ready(function()
 			if(direction == "up" || direction == "down")
 				return;
 			context.clearRect(0,0,gameArea.width,gameArea.height);
+			drawFruit(xPosition,yPosition);
 			snakeXPositions.shift();
 			snakeYPositions.shift();
 			redrawSnake();
@@ -90,6 +118,7 @@ $(document).ready(function()
 			if(direction == "right" || direction == "left")
 				return;
 			context.clearRect(0,0,gameArea.width,gameArea.height);
+			drawFruit(xPosition,yPosition);
 			snakeXPositions.shift();
 			snakeYPositions.shift();
 			redrawSnake();
@@ -113,6 +142,7 @@ $(document).ready(function()
 			if(direction == "down" || direction == "up")
 				return;
 			context.clearRect(0,0,gameArea.width,gameArea.height);
+			drawFruit(xPosition,yPosition);
 			snakeXPositions.shift();
 			snakeYPositions.shift();
 			redrawSnake();
@@ -146,6 +176,7 @@ $(document).ready(function()
     {
 		var temp = 0;
 		context.clearRect(0,0,gameArea.width,gameArea.height);
+		drawFruit(xPosition,yPosition);
 		if(direction == "left")
 		{
 			snakeXPositions.shift();
@@ -248,4 +279,12 @@ $(document).ready(function()
 		}
 		setTimeout(move,time);
     }
+	function drawFruit(x,y)
+	{
+		context.fillStyle = FRUIT_COLOR;
+		context.beginPath();
+		context.arc(x,y,7.5,0,2 * Math.PI);
+		context.fill();
+		context.fillStyle = SNAKE_COLOR;
+	}
 });
